99b522f82573045b514d7dbed07a90a4b0d77963
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2020-06-30 19:55:55 +0800

    add

--- MyOpenGLStudy01/_36_Bloom.cpp
+++ MyOpenGLStudy01/_36_Bloom.cpp
@@ -1,7 +1,108 @@
 ﻿#include "_36_Bloom.h"
+#include "CommonBaseScript.h"
+#include "Camera.h"
+#include "ImageHelper.h"
 
 int _36_Bloom::DoMain()
 {
+	CommonBaseScript::InitOpenGL();
+	GLFWwindow* window = CommonBaseScript::InitWindow();
+
+	if (window == nullptr)
+	{
+		return -1;
+	}
+
+	// configure (floating point) framebuffers
+	// ---------------------------------------
+	unsigned int hdrFBO;
+	glGenFramebuffers(1, &hdrFBO);
+	glBindFramebuffer(GL_FRAMEBUFFER, hdrFBO);
+	//创建两张图 一张是 正常的图  一张是模糊过亮区域的图
+	unsigned int colorBuffers[2];
+	glGenTextures(2, colorBuffers);
+	for (unsigned int i = 0; i < 2; i++)
+	{
+		glBindTexture(GL_TEXTURE_2D, colorBuffers[i]);
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA16F, SCR_WIDTH, SCR_HEIGHT, 0, GL_RGB, GL_FLOAT, nullptr);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE); //这里需要Clamp Edge  不然模糊采样会跳边缘
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+		glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0 + i, GL_TEXTURE_2D, colorBuffers[i], 0);
+	}
+	//create and attach depth buffer
+	unsigned int rboDepth;
+	glGenRenderbuffers(1, &rboDepth);
+	glBindRenderbuffer(GL_RENDERBUFFER, rboDepth);
+	glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT, SCR_WIDTH, SCR_HEIGHT);
+	glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, rboDepth);
+	//告诉opengl 要画哪几个MRT  默认是 MRT0
+	unsigned int attachments[2] = {GL_COLOR_ATTACHMENT0,GL_COLOR_ATTACHMENT1};
+	glDrawBuffers(2, attachments);
+	//final check if framebuffer is  complete
+	if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+	{
+		std::cout << "Framebuffer not complete!" << std::endl;
+	}
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+	unsigned int pingpongFBO[2];
+	unsigned int pingpongColorbuffers[2];
+	glGenFramebuffers(2, pingpongFBO);
+	glGenTextures(2, pingpongColorbuffers);
+	for (unsigned int i = 0; i < 2; i++)
+	{
+		glBindFramebuffer(GL_FRAMEBUFFER, pingpongFBO[i]);
+		glBindTexture(GL_TEXTURE_2D, pingpongColorbuffers[i]);
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB16F, SCR_WIDTH, SCR_HEIGHT, 0, GL_RGB, GL_FLOAT, nullptr);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE); //这里需要Clamp Edge  不然模糊采样会跳边缘
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+		glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, pingpongColorbuffers[i], 0);
+		// also check if framebuffers are complete (no need for depth buffer)
+		if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+			std::cout << "Framebuffer not complete!" << std::endl;
+	}
+
+
+	//Camera
+	//-----------------
+	Camera camera{};
+	Camera::AddMouseEvent(window);
+	CommonBaseScript::RegisterKeyEvent(window);
+
+	glEnable(GL_DEPTH_TEST);
+
+	//Shader
+	//-----------------
+	Shader objShader{};
+	Shader lightShader{};
+	Shader blurShader{};
+	Shader bloomFinalShader{};
+
+	//Textures
+	//---------------
+	unsigned int woodTexture = ImageHelper::LoadTexture("");
+	unsigned int containerTexture = ImageHelper::LoadTexture("");
+
+	//Shader Configuration
+	//-------------------
+	objShader.Use()
+	objShader.setInit//TODO:
+
+	while (!glfwWindowShouldClose(window))
+	{
+		CommonBaseScript::ProcessInput(window);
+		CommonBaseScript::ProcessKeyClick();
+		camera.DoKeyboardMove(window);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
 
 	return 0;
 }
