137c1a564075247dde7a1ef4998e57efd45228bf
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2020-07-21 19:53:15 +0800

    add

--- MyOpenGLStudy01/_38_DeferredShading_Volumes.cpp
+++ MyOpenGLStudy01/_38_DeferredShading_Volumes.cpp
@@ -1,14 +1,14 @@
-﻿#include "_37_DeferredShading.h"
+﻿#include "_38_DeferredShading_Volumes.h"
 
 #include "CommonBaseScript.h"
 #include "Camera.h"
 #include "ImageHelper.h"
 #include "Model.h"
 
-unsigned int _37_DeferredShading::cubeVAO = 0;
-unsigned int _37_DeferredShading::quadVAO = 0;
+unsigned int _38_DeferredShading_Volumes::cubeVAO = 0;
+unsigned int _38_DeferredShading_Volumes::quadVAO = 0;
 
-int _37_DeferredShading::DoMain()
+int _38_DeferredShading_Volumes::DoMain()
 {
 	CommonBaseScript::InitOpenGL();
 	GLFWwindow* window = CommonBaseScript::InitWindow();
@@ -21,14 +21,12 @@ int _37_DeferredShading::DoMain()
 	BindCubeVAO();
 	BindQuadVAO();
 
-
 	// build and compile shaders
 	//--------------------------
-	Shader geometryPassShader{"37_GeometryPass"};
-	Shader lightingPassShader{"37_DeferredLighting"};
-	Shader lightBoxShader{"37_LightBox"};
-	Shader fboDebugShader{ "37_FBODebug" };
-
+	Shader geometryPassShader{"38_GeometryPass"};
+	Shader lightingPassShader{"38_DeferredLighting"};
+	Shader lightBoxShader{"38_LightBox"};
+	Shader fboDebugShader{"38_FBODebug"};
 
 	//load models
 	//--------------
@@ -44,24 +42,19 @@ int _37_DeferredShading::DoMain()
 	objectPositions.emplace_back(glm::vec3(0.0, -3.0, 3.0));
 	objectPositions.emplace_back(glm::vec3(3.0, -3.0, 3.0));
 
-
 	//configure g-buffer framebuffer
-	//--------------------------------
+	//--------------------
 	unsigned int gBuffer;
 	glGenFramebuffers(1, &gBuffer);
 	glBindFramebuffer(GL_FRAMEBUFFER, gBuffer);
 	unsigned int gPosition, gNormal, gAlbedoSpec;
-
 	//position color buffer
 	glGenTextures(1, &gPosition);
 	glBindTexture(GL_TEXTURE_2D, gPosition);
-	//16f 用于储存worldPosition
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB16F, SCR_WIDTH, SCR_HEIGHT, 0, GL_RGB, GL_FLOAT, nullptr);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-	//最后一位指定要附加的纹理图像的mipmap级别，该级别必须为0。
 	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, gPosition, 0);
-
 	//normal color buffer
 	glGenTextures(1, &gNormal);
 	glBindTexture(GL_TEXTURE_2D, gNormal);
@@ -69,61 +62,53 @@ int _37_DeferredShading::DoMain()
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT1, GL_TEXTURE_2D, gNormal, 0);
-
-	//color + specular color buffer
+	//color+specular color buffer
 	glGenTextures(1, &gAlbedoSpec);
 	glBindTexture(GL_TEXTURE_2D, gAlbedoSpec);
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, SCR_WIDTH, SCR_HEIGHT, 0, GL_RGBA, GL_UNSIGNED_BYTE, nullptr);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT2, GL_TEXTURE_2D, gAlbedoSpec, 0);
-
-	//MRT
-	unsigned int attachments[3] = {GL_COLOR_ATTACHMENT0,GL_COLOR_ATTACHMENT1, GL_COLOR_ATTACHMENT2,};
+	//attachment
+	unsigned int attachments[3] = {GL_COLOR_ATTACHMENT0,GL_COLOR_ATTACHMENT1,GL_COLOR_ATTACHMENT2};
 	glDrawBuffers(3, attachments);
-
-	//depth rt
+	//create depth buffer
 	unsigned int rboDepth;
 	glGenRenderbuffers(1, &rboDepth);
 	glBindRenderbuffer(GL_RENDERBUFFER, rboDepth);
 	glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT, SCR_WIDTH, SCR_HEIGHT);
 	glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, rboDepth);
-	
+	//final check
 	if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
-	{
-		std::cout << "Frame not complete!" << std::endl;
-	}
+		std::cout << "Framebuffer not complete" << std::endl;
 	glBindFramebuffer(GL_FRAMEBUFFER, 0);
 
 	// lighting info
-	//-------------------
+	// -------------
 	const unsigned int NR_LIGHTS = 32;
 	std::vector<glm::vec3> lightPositions;
 	std::vector<glm::vec3> lightColors;
 	srand(13);
 	for (unsigned int i = 0; i < NR_LIGHTS; i++)
 	{
-		//calculate slightly random offsets
+		// calculate slightly random offsets
 		float xPos = ((rand() % 100) / 100.0) * 6.0 - 3.0;
-		float yPos = ((rand() % 100) / 100.0) * 6.0 - 3.0;
+		float yPos = ((rand() % 100) / 100.0) * 6.0 - 4.0;
 		float zPos = ((rand() % 100) / 100.0) * 6.0 - 3.0;
-		lightPositions.emplace_back(glm::vec3(xPos, yPos, zPos));
-		//also calculate random color
-		float rColor = ((rand() % 100) / 200.0f) + 0.5; //[0.5 , 1.0]
-		float gColor = ((rand() % 100) / 200.0f) + 0.5; //[0.5 , 1.0]
-		float bColor = ((rand() % 100) / 200.0f) + 0.5; //[0.5 , 1.0]
-		lightColors.emplace_back(glm::vec3(rColor, gColor, bColor));
+		lightPositions.push_back(glm::vec3(xPos, yPos, zPos));
+		// also calculate random color
+		float rColor = ((rand() % 100) / 200.0f) + 0.5; // between 0.5 and 1.0
+		float gColor = ((rand() % 100) / 200.0f) + 0.5; // between 0.5 and 1.0
+		float bColor = ((rand() % 100) / 200.0f) + 0.5; // between 0.5 and 1.0
+		lightColors.push_back(glm::vec3(rColor, gColor, bColor));
 	}
 
-	//shader configuration
-	//---------------------
+	// shader configuration
+	// --------------------
 	lightingPassShader.Use();
 	lightingPassShader.SetInt("gPosition", 0);
 	lightingPassShader.SetInt("gNormal", 1);
 	lightingPassShader.SetInt("gAlbedoSpec", 2);
-
-	fboDebugShader.Use();
-	lightingPassShader.SetInt("fboAttachment", 0);
 
 	//Camera
 	//-----------------
@@ -141,21 +126,15 @@ int _37_DeferredShading::DoMain()
 		CommonBaseScript::ProcessKeyClick();
 		camera.DoKeyboardMove(window);
 
-		//render
-		//---------------
 		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-		//1.geometry pass:: render scene's geometry/color data into gbuffer
+		//1.geometry buffer
 		glBindFramebuffer(GL_FRAMEBUFFER, gBuffer);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		glm::mat4 projection = camera.GetProjectionMat4();
-		glm::mat4 view = camera.GetViewMat4();
-		glm::mat4 model = glm::mat4(1.0f);
-
 		geometryPassShader.Use();
-		geometryPassShader.SetMat4("projection", projection);
-		geometryPassShader.SetMat4("view", view);
+		geometryPassShader.SetMat4("projection", camera.GetProjectionMat4());
+		geometryPassShader.SetMat4("view", camera.GetViewMat4());
+		glm::mat4 model = glm::mat4(1.0f);
 		for (auto objectPosition : objectPositions)
 		{
 			model = glm::mat4(1.0f);
@@ -165,9 +144,7 @@ int _37_DeferredShading::DoMain()
 			nanosuit.Draw(geometryPassShader);
 		}
 
-
-		//2.lighting pass: calculate lighting by iterating over a screen filled quad pixel-by-pixel using the gbuffer's content
-		//------------------------------
+		//2.lighting pass
 		glBindFramebuffer(GL_FRAMEBUFFER, 0);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		glActiveTexture(GL_TEXTURE0);
@@ -176,43 +153,20 @@ int _37_DeferredShading::DoMain()
 		glBindTexture(GL_TEXTURE_2D, gNormal);
 		glActiveTexture(GL_TEXTURE2);
 		glBindTexture(GL_TEXTURE_2D, gAlbedoSpec);
-		lightingPassShader.Use();
-		//send light relevant uniforms
-		for (unsigned int i = 0; i < lightPositions.size(); i++)
+		for (unsigned int i = 0; i < lightPositions.size(); ++i)
 		{
-			lightingPassShader.SetVec3("lights[" + std::to_string(i) + "].Position", lightPositions[i]);
-			lightingPassShader.SetVec3("lights[" + std::to_string(i) + "].Color", lightColors[i]);
-			//变量
-			const float constant = 1.0f; //注意我们不同发送这个给shader , 我们只用知道他永远是 1 在这个demo里面
+			std::string lightName = "lights[" + std::to_string(i) + "].";
+
+			lightingPassShader.SetVec3(lightName + "Position", lightPositions[i]);
+			lightingPassShader.SetVec3(lightName + "Color", lightColors[i]);
+
+			const float constant = 1.0; // 我们只用知道这是1.0 并且写死在shader里面
 			const float linear = 0.7;
 			const float quadratic = 1.8;
-			lightingPassShader.SetFloat("lights[" + std::to_string(i) + "].Linear", linear);
-			lightingPassShader.SetFloat("lights[" + std::to_string(i) + "].Quadratic", quadratic);
-		}
-		lightingPassShader.SetVec3("viewPos", camera.position);
-		//render quad
-		RenderQuad();
+			lightingPassShader.SetFloat(lightName + "Linear", linear);
+			lightingPassShader.SetFloat(lightName + "Quadratic", quadratic);
 
-
-		//2.5 复制深度 为了后面的遮挡
-		glBindFramebuffer(GL_READ_FRAMEBUFFER, gBuffer);
-		glBindFramebuffer(GL_DRAW_BUFFER, 0); //写入默认 framebuffer
-		//blit到默认帧缓冲区。请注意，由于FBO和默认帧缓冲区的内部格式必须匹配，这可能也可能不起作用。
-		glBlitFramebuffer(0, 0, SCR_WIDTH, SCR_HEIGHT, 0, 0, SCR_WIDTH, SCR_HEIGHT, GL_DEPTH_BUFFER_BIT, GL_NEAREST);
-		glBindFramebuffer(GL_FRAMEBUFFER, 0);
-
-		//3.渲染灯光在场景里面
-		lightBoxShader.Use();
-		lightBoxShader.SetMat4("projection", projection);
-		lightBoxShader.SetMat4("view", view);
-		for(unsigned int i=0;i<lightPositions.size();i++)
-		{
-			model = glm::mat4(1.0f);
-			model = glm::translate(model, lightPositions[i]);
-			model = glm::scale(model, glm::vec3(0.125f));
-			lightBoxShader.SetMat4("model", model);
-			lightBoxShader.SetVec3("lightColor", lightColors[i]);
-			RenderCube();
+			//calc radius of light volume/sphere
 		}
 
 		glfwSwapBuffers(window);
@@ -224,7 +178,7 @@ int _37_DeferredShading::DoMain()
 	return 0;
 }
 
-void _37_DeferredShading::BindCubeVAO()
+void _38_DeferredShading_Volumes::BindCubeVAO()
 {
 	float vertices[] = {
 		// back face
@@ -288,7 +242,7 @@ void _37_DeferredShading::BindCubeVAO()
 	glBindVertexArray(0);
 }
 
-void _37_DeferredShading::RenderCube()
+void _38_DeferredShading_Volumes::RenderCube()
 {
 	//render Cube
 	glBindVertexArray(cubeVAO);
@@ -297,7 +251,7 @@ void _37_DeferredShading::RenderCube()
 }
 
 
-void _37_DeferredShading::BindQuadVAO()
+void _38_DeferredShading_Volumes::BindQuadVAO()
 {
 	float quadVertices[] = {
 		// positions        // texture Coords
@@ -319,7 +273,7 @@ void _37_DeferredShading::BindQuadVAO()
 	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), reinterpret_cast<void*>(3 * sizeof(float)));
 }
 
-void _37_DeferredShading::RenderQuad()
+void _38_DeferredShading_Volumes::RenderQuad()
 {
 	glBindVertexArray(quadVAO);
 	glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
---
+++ MyOpenGLStudy01/_38_DeferredShading_Volumes.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+
+class _38_DeferredShading_Volumes
+{
+public:
+	static unsigned int cubeVAO;
+	static unsigned int quadVAO;
+
+	static int DoMain();
+	static void BindCubeVAO();
+	static void RenderCube();
+	static void BindQuadVAO();
+	static void RenderQuad();
+};
