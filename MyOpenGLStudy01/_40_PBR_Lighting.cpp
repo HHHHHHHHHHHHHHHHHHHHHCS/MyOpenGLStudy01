6c6a5e9383d419e9b681f41b09f8691c7634f602
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2020-08-05 19:51:20 +0800

    add

--- MyOpenGLStudy01/_40_PBR_Lighting.cpp
+++ MyOpenGLStudy01/_40_PBR_Lighting.cpp
@@ -3,24 +3,83 @@
 #include <vector>
 #include <vec3.hpp>
 #include <vec2.hpp>
+#include "CommonBaseScript.h"
+#include "Shader.h"
+#include "Camera.h"
 
+class Camera;
 unsigned int _40_PBR_Lighting::sphereVAO = 0;
 unsigned int _40_PBR_Lighting::sphereIndexCount = 0;
 
 int _40_PBR_Lighting::DoMain()
 {
+	CommonBaseScript::InitOpenGL();
+	GLFWwindow* window = CommonBaseScript::InitWindow();
+
+	if (window == nullptr)
+	{
+		return -1;
+	}
+
+	BindSphereVAO();
+
+	Shader shader{"40_PBR_Lighting"};
+
+
+	//lights
+	glm::vec3 lightPositions[] = {
+		glm::vec3(-10.0f, 10.0f, 10.0f),
+		glm::vec3(10.0f, 10.0f, 10.0f),
+		glm::vec3(-10.0f, -10.0f, 10.0f),
+		glm::vec3(10.0f, -10.0f, 10.0f),
+	};
+	glm::vec3 lightColors[] = {
+		glm::vec3(300.0f, 300.0f, 300.0f),
+		glm::vec3(300.0f, 300.0f, 300.0f),
+		glm::vec3(300.0f, 300.0f, 300.0f),
+		glm::vec3(300.0f, 300.0f, 300.0f),
+	};
+
+	//Camera
+	//-----------
+	Camera camera{};
+	Camera::AddMouseEvent(window);
+	CommonBaseScript::RegisterKeyEvent(window);
+
+
+	shader.Use();
+	shader.SetVec3("albedo", 0.5f, 0.0f, 0.0f);
+	shader.SetFloat("ao", 1.0f);
+	shader.SetMat4("projection", camera.GetProjectionMat4());
+
+	glEnable(GL_DEPTH_TEST);
+
+	int nrRows = 7;
+	int nrColumns = 7;
+	float spacing = 2.5;
+
+	while (!glfwWindowShouldClose(window))
+	{
+		CommonBaseScript::ProcessInput(window);
+		CommonBaseScript::ProcessKeyClick();
+		camera.DoKeyboardMove(window);
+
+		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+
 	return 0;
 }
 
 
 void _40_PBR_Lighting::BindSphereVAO()
 {
-	glGenVertexArrays(1, &sphereVAO);
-
-	unsigned int vbo, ebo;
-	glGenBuffers(1, &vbo);
-	glGenBuffers(1, &ebo);
-
 	std::vector<glm::vec3> positions;
 	std::vector<glm::vec2> uv;
 	std::vector<glm::vec3> normals;
@@ -49,10 +108,10 @@ void _40_PBR_Lighting::BindSphereVAO()
 	}
 
 	//4个点画一个quad 错行 合成
-	bool oddRow = false;//奇数行
+	bool oddRow = false; //奇数行
 	for (int y = 0; y < Y_SEGMENTS; ++y)
 	{
-		if (!oddRow)//偶数行
+		if (!oddRow) //偶数行
 		{
 			for (int x = 0; x <= X_SEGMENTS; ++x)
 			{
@@ -71,4 +130,59 @@ void _40_PBR_Lighting::BindSphereVAO()
 		oddRow = !oddRow;
 	}
 
+	std::vector<float> data;
+	for (int i = 0; i < positions.size(); ++i)
+	{
+		data.push_back(positions[i].x);
+		data.push_back(positions[i].y);
+		data.push_back(positions[i].z);
+
+		if (!uv.empty())
+		{
+			data.push_back(uv[i].x);
+			data.push_back(uv[i].y);
+		}
+		if (!normals.empty())
+		{
+			data.push_back(normals[i].x);
+			data.push_back(normals[i].y);
+			data.push_back(normals[i].z);
+		}
+	}
+
+	glGenVertexArrays(1, &sphereVAO);
+
+	unsigned int vbo;
+	glGenBuffers(1, &vbo);
+	glBindVertexArray(sphereVAO);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, data.size() * sizeof(float), &data[0], GL_STATIC_DRAW);
+
+	unsigned int ebo;
+	glGenBuffers(1, &ebo);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(int), &indices[0], GL_STATIC_DRAW);
+
+	const unsigned int stride = (3 + 2 + 3) * sizeof(float);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, stride, reinterpret_cast<void*>(0));
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, stride, reinterpret_cast<void*>(3 * sizeof(float)));
+	glEnableVertexAttribArray(2);
+	glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, stride, reinterpret_cast<void*>(5 * sizeof(float)));
+}
+
+void _40_PBR_Lighting::RenderSphere()
+{
+	glBindVertexArray(sphereVAO);
+
+	// https://blog.csdn.net/vblegend_2013/article/details/85345102
+	// GL_TRIANGLE_STRIP则稍微有点复杂。
+	// 其规律是：
+	// 构建当前三角形的顶点的连接顺序依赖于要和前面已经出现过的2个顶点组成三角形的当前顶点的序号的奇偶性（如果从0开始）：
+	// 如果当前顶点是奇数：
+	// 组成三角形的顶点排列顺序：T = [n - 1 n - 2 n].
+	// 如果当前顶点是偶数：
+	// 组成三角形的顶点排列顺序：T = [n - 2 n - 21 n].
+	glDrawElements(GL_TRIANGLE_STRIP, sphereIndexCount, GL_UNSIGNED_INT, nullptr);
 }
