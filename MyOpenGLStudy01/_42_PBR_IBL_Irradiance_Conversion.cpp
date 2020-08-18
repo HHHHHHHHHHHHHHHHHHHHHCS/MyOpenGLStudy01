3126eff56562b924e4bd2e5c64ac48a52f8acda8
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2020-08-18 19:53:19 +0800

    add

---
+++ MyOpenGLStudy01/_42_PBR_IBL_Irradiance_Conversion.cpp
@@ -0,0 +1,18 @@
+﻿#include "_42_PBR_IBL_Irradiance_Conversion.h"
+#include "CommonBaseScript.h"
+
+int _42_PBR_IBL_Irradiance_Conversion::DoMain()
+{
+	CommonBaseScript::InitOpenGL();
+	GLFWwindow* window = CommonBaseScript::InitWindow();
+
+	if(window == nullptr)
+	{
+		return -1;
+	}
+
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LEQUAL);//天空盒深度是1  小于等于1 的被渲染
+
+	//TODO:
+}
---
+++ MyOpenGLStudy01/_42_PBR_IBL_Irradiance_Conversion.h
@@ -0,0 +1,7 @@
+﻿#pragma once
+
+class _42_PBR_IBL_Irradiance_Conversion
+{
+public:
+	static int DoMain();
+};
