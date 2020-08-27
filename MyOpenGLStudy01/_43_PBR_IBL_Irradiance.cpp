1e2fd34c0d6d7d979a1ae1a25a8e6fd0334849e7
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2020-08-27 19:55:28 +0800

    add

---
+++ MyOpenGLStudy01/_43_PBR_IBL_Irradiance.cpp
@@ -0,0 +1,131 @@
+﻿#include "_43_PBR_IBL_Irradiance.h"
+#include "CommonBaseScript.h"
+#include "Camera.h"
+#include "ImageHelper.h"
+
+int _43_PBR_IBL_Irradiance::DoMain()
+{
+	CommonBaseScript::InitOpenGL();
+	GLFWwindow* window = CommonBaseScript::InitWindow();
+
+	if (window == nullptr)
+	{
+		return -1;
+	}
+
+	Camera camera{};
+	Camera::AddMouseEvent(window);
+	CommonBaseScript::RegisterKeyEvent(window);
+
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LEQUAL);
+
+	Shader pbrShader{""};
+	Shader equirectangularToCubemapShader{""};
+	Shader irradianceShader{""};
+	Shader backgroundShader{""};
+
+	unsigned int hdrTexture = ImageHelper::LoadHDR_Filp("hdr/newport_loft.hdr");
+
+	unsigned int captureFBO;
+	glGenFramebuffers(1, &captureFBO);
+	glBindFramebuffer(GL_FRAMEBUFFER, captureFBO);
+	unsigned int captureRBO;
+	glGenRenderbuffers(1, &captureRBO);
+	glBindRenderbuffer(GL_RENDERBUFFER, captureRBO);
+	glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT24, 512, 512);
+	glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, captureRBO);
+	
+	unsigned int envCubemap;
+	glGenTextures(1, &envCubemap);
+	glBindTexture(GL_TEXTURE_CUBE_MAP, envCubemap);
+	for(unsigned int i=0;i<6;++i)
+	{
+		//HDR RGB16F
+		glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, GL_RGB16F, 512, 512, 0, GL_RGB, GL_FLOAT, nullptr);
+	}
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	glm::mat4 captureProjection = glm::perspective(glm::radians(90.0f), 1.0f, 0.1f, 10.0f);
+	glm::mat4 captureViews[] =
+	{
+		glm::lookAt(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(1.0f,  0.0f,  0.0f), glm::vec3(0.0f, -1.0f,  0.0f)),
+		glm::lookAt(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(-1.0f,  0.0f,  0.0f), glm::vec3(0.0f, -1.0f,  0.0f)),
+		glm::lookAt(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f,  1.0f,  0.0f), glm::vec3(0.0f,  0.0f,  1.0f)),
+		glm::lookAt(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, -1.0f,  0.0f), glm::vec3(0.0f,  0.0f, -1.0f)),
+		glm::lookAt(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f,  0.0f,  1.0f), glm::vec3(0.0f, -1.0f,  0.0f)),
+		glm::lookAt(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f,  0.0f, -1.0f), glm::vec3(0.0f, -1.0f,  0.0f))
+	};
+
+	equirectangularToCubemapShader.Use();
+	equirectangularToCubemapShader.SetInt("equirectangularMap", 0);
+	equirectangularToCubemapShader.SetMat4("projection", captureProjection);
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, hdrTexture);
+
+	glViewport(0, 0, 512, 512); // don't forget to configure the viewport to the capture dimensions.
+	glBindFramebuffer(GL_FRAMEBUFFER, captureFBO);
+	for (unsigned int i = 0; i < 6; ++i)
+	{
+		equirectangularToCubemapShader.SetMat4("view", captureViews[i]);
+		glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, envCubemap, 0);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		//TODO:
+		//renderCube();
+	}
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+
+
+
+
+
+
+
+	glm::vec3 lightPositions[] = {
+		glm::vec3(-10.0f, 10.0f, 10.0f),
+		glm::vec3(10.0f, 10.0f, 10.0f),
+		glm::vec3(-10.0f, -10.0f, 10.0f),
+		glm::vec3(10.0f, -10.0f, 10.0f),
+	};
+	glm::vec3 lightColors[] = {
+		glm::vec3(300.0f, 300.0f, 300.0f),
+		glm::vec3(300.0f, 300.0f, 300.0f),
+		glm::vec3(300.0f, 300.0f, 300.0f),
+		glm::vec3(300.0f, 300.0f, 300.0f),
+	};
+	int nrRows = 7;
+	int nrColumns = 7;
+	float spacing = 2.5;
+
+	pbrShader.Use();
+	pbrShader.SetInt("irradianceMap", 0);
+	pbrShader.SetVec3("albedo", 0.5f, 0.0f, 0.0f);
+	pbrShader.SetFloat("ao", 1.0f);
+
+	backgroundShader.Use();
+	backgroundShader.SetInt("environmentMap", 0);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		CommonBaseScript::ProcessInput(window);
+		CommonBaseScript::ProcessKeyClick();
+		camera.DoKeyboardMove(window);
+
+		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+
+	return 0;
+}
---
+++ MyOpenGLStudy01/_43_PBR_IBL_Irradiance.h
@@ -0,0 +1,7 @@
+﻿#pragma once
+
+class _43_PBR_IBL_Irradiance
+{
+public:
+	static int DoMain();
+};
