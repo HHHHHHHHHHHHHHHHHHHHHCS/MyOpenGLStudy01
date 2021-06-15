c8680575bd31d0497e2b1a96ded9634129ea64c3
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2021-06-15 21:07:42 +0800

    add

--- MyOpenGLStudy01/Bone.cpp
+++ MyOpenGLStudy01/Bone.cpp
@@ -1,1 +1,101 @@
 ﻿#include "Bone.h"
+
+#include "AssimpGLMHelpers.h"
+
+Bone::Bone(const std::string& _name, int _id, const aiNodeAnim* _channel)
+	: localTransform(1.0f), name(_name), id(_id)
+{
+	numPositions = _channel->mNumPositionKeys;
+
+	for (int positionIndex = 0; positionIndex < numPositions; positionIndex++)
+	{
+		aiVector3D aiPosition = _channel->mPositionKeys[positionIndex].mValue;
+		float timeStamp = _channel->mPositionKeys[positionIndex].mTime;
+		KeyPosition data;
+		data.position = AssimpGLMHelpers::GetGLMVec(aiPosition);
+		data.timeStamp = timeStamp;
+		positions.push_back(data);
+	}
+
+	numRotations = _channel->mNumRotationKeys;
+	for (int rotationIndex = 0; rotationIndex < numRotations; rotationIndex++)
+	{
+		aiQuaternion aiQuaternion = _channel->mRotationKeys[rotationIndex].mValue;
+		float timeStamp = _channel->mRotationKeys[rotationIndex].mTime;
+		KeyRotation data;
+		data.orientation = AssimpGLMHelpers::GetGLMQuat(aiQuaternion);
+		data.timeStamp = timeStamp;
+		rotations.push_back(data);
+	}
+
+	numScalings = _channel->mNumScalingKeys;
+	for (int keyIndex = 0; keyIndex < numScalings; keyIndex++)
+	{
+		aiVector3D scale = _channel->mScalingKeys[keyIndex].mValue;
+		float timeStamp = _channel->mScalingKeys[keyIndex].mTime;
+		KeyScale data;
+		data.scale = AssimpGLMHelpers::GetGLMVec(scale);
+		data.timeStamp = timeStamp;
+		scales.push_back(data);
+	}
+}
+
+void Bone::Update(float animationTime)
+{
+	glm::mat4 translation = InterpolatePosition(animationTime);
+	glm::mat4 rotation = InterpolatePosition(animationTime);
+	glm::mat4 scale = InterpolatePosition(animationTime);
+	localTransform = translation * rotation * scale;
+}
+
+glm::mat4 Bone::GetLocalTransform() const
+{
+	return localTransform;
+}
+
+std::string Bone::GetBoneName() const
+{
+	return name;
+}
+
+int Bone::GetBoneID() const
+{
+	return id;
+}
+
+
+int Bone::GetPositionIndex(float animationTime) const
+{
+	for (int index = 0; index < numPositions - 1; index++)
+	{
+		if (animationTime < positions[index + 1].timeStamp)
+		{
+			return index;
+		}
+	}
+	assert(0);
+}
+
+int Bone::GetRotationIndex(float animationTime) const
+{
+	for (int index = 0; index < numRotations - 1; index++)
+	{
+		if (animationTime < rotations[index + 1].timeStamp)
+		{
+			return index;
+		}
+	}
+	assert(0);
+}
+
+int Bone::GetScaleIndex(float animationTime) const
+{
+	for (int index = 0; index < numScalings - 1; index++)
+	{
+		if (animationTime < scales[index + 1].timeStamp)
+		{
+			return index;
+		}
+	}
+	assert(0);
+}
--- MyOpenGLStudy01/Bone.h
+++ MyOpenGLStudy01/Bone.h
@@ -1,6 +1,7 @@
 ﻿#pragma once
 #include <string>
 #include <vec3.hpp>
+#include <vector>
 #include <assimp/anim.h>
 #include <gtx/quaternion.hpp>
 
@@ -25,7 +26,33 @@ struct KeyScale
 class Bone
 {
 public:
-	Bone(const std::string& name, int ID, const aiNodeAnim* channel);
+	Bone(const std::string& _name, int _id, const aiNodeAnim* _channel);
 
 	void Update(float animationTime);
+
+	glm::mat4 GetLocalTransform() const;
+	std::string GetBoneName() const;
+	int GetBoneID() const;
+
+	int GetPositionIndex(float animationTime) const;
+	int GetRotationIndex(float animationTime) const;
+	int GetScaleIndex(float animationTime) const;
+
+private:
+	std::vector<KeyPosition> positions;
+	std::vector<KeyRotation> rotations;
+	std::vector<KeyScale> scales;
+	int numPositions;
+	int numRotations;
+	int numScalings;
+
+	glm::mat4 localTransform;
+	std::string name;
+	int id;
+
+	float GetScaleFactor(float lastTimeStamp, float nextTimeStamp, float animationTime);
+
+	glm::mat4 InterpolatePosition(float animationTime);
+	glm::mat4 InterpolateRotation(float animationTime);
+	glm::mat4 InterpolateScale(float animationTime);
 };
